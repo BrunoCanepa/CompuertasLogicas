0e042c5b6991e45e7d6bad7262684009edfdac5d
BrunoCanepa <brunocanepa22@gmail.com>
2022-12-12 14:20:38 -0300

    fix

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,10 +8,11 @@ public class Program
         CompuertaAND and1 = new CompuertaAND("AND-1");
         and1.AgregarEntrada("A", True);
         and1.AgregarEntrada("B", True);
+
         CompuertaOR or2 = new CompuertaOR("OR-2");
         or2.AgregarEntrada("C", False);
         or2.AgregarEntrada(and1.Nombre, and1);
-        Console.WriteLine(or2.Calcular());
+
         CompuertaNOT not3 = new CompuertaNOT("NOT-3");
         not3.AgregarEntrada(or2.Nombre, or2);
         Console.WriteLine(not3.Calcular());
